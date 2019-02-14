5b774d04e2ba4fc15ba5b05e25ac1c6cc30ee74b
felixf <felix@thealbatross.co.uk>
2019-02-14 13:29:42 +0000

    oops

--- javascript.js
+++ javascript.js
@@ -3,12 +3,12 @@ var disclaimers = ["Hey, I haven't obscured this at all, so welcome to the code.
 "I kept these mostly gender-neutral but there's a general male-speaking-to-female dynamic so watch out, don't put all your hopes in this basket"];
 var setups = ['Are you my little toe?',
 'Are you heroin?',
-'Is your name Luke?'
+'Is your name Luke?',
 'C7',
 "Are you not-sand?",
 'Please have sex with me?',
 'Have you heard of "Imagine Dragons"?',
-'Hey, want some updog?'
+'Hey, want some updog?',
 "How much does a polar bear weigh?",
 "Don't you agree, pens should be like women?",
 "Bags should be like women.",
@@ -17,11 +17,11 @@ var setups = ['Are you my little toe?',
 var responses = ['Because I want to bang you on all my furniture',
 "Because despite everything I've been told, I wanna do you",
 "Because I'm your daddy",
-"Oh sorry, I thought this was a vending machine - because you're a snack!"
+"Oh sorry, I thought this was a vending machine - because you're a snack!",
 "Because I don't like you. You're not coarse and not rough and not irritating and don't get everywhere",
 "I'm very lonely and you're so attractive",
 "Imagine draggin' these balls across your face",
-"Nothing much dog, what's up with you?"
+"Nothing much dog, what's up with you?",
 "Not enough to break the ice - if polar bears did weigh that much they'd be in trouble",
 "Disposable.",
 "Simple and with one lockable opening",
